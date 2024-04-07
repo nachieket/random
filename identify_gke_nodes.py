f37c375b784c6ee806b2ed11ae1a1fb587c4138e
nachiket <nachiketj@outlook.com>
2024-04-07 21:05:56 +0100

    07/04/2023:01

--- identify_gke_nodes.py
+++ identify_gke_nodes.py
@@ -4,74 +4,81 @@ Requires gcloud CLI to be installed and configured.
 """
 
 import subprocess
-import re
+import json
 
 
 def get_all_gcp_projects():
   """
   Executes a gcloud command to list all project IDs and returns them as a list.
   """
-  command = 'gcloud projects list --format="text(projectId)"'
+  projects = []
+
+  command = 'gcloud projects list --format="json(projectId)"'
   process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
   output, err = process.communicate()
+
   if err:
     raise RuntimeError(f"Error running gcloud command: {err.decode()}")
-  return output.decode().splitlines()
+  raw_data = json.loads(output.decode())
+
+  for data in raw_data:
+    projects.append(data['projectId'])
+
+  return projects
 
 
 def main():
   try:
-    projects = get_all_gcp_projects()
-    print(f"Found {len(projects)} GCP projects\n")
-    names = []
-    total_cos_nodes = 0
+    total_standard_nodes = 0
     total_autopilot_nodes = 0
 
-    for project in projects:
-      if "project_id" in project:
-        names.append(project.split(' ')[1])
+    projects = get_all_gcp_projects()
 
-    for name in names:
+    print(f"Found {len(projects)} GCP projects\n")
+
+    for project in projects:
       try:
-        command = f'gcloud container clusters list --project {name}'
+        print(f'###### Project Name: {project} #########\n')
+
+        command = f'gcloud container clusters list --project {project} --format="json()"'
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
         output, err = process.communicate()
-        clusters = output.split('\n\n')
 
-        if output:
-          pattern = r"(NAME|LOCATION|NUM_NODES): (.*)"
+        output = json.loads(output)
 
-          for x in clusters:
-            results = {}
-            for match in re.finditer(pattern, x, flags=re.MULTILINE):
-              key = match.group(1).lower()  # Convert key to lowercase for consistency
-              value = match.group(2).strip()  # Remove leading/trailing whitespace
-              results[key] = value
+        if len(output) < 1:
+          print(f'Project {project} has no GKE clusters. Skipping.\n')
+          continue
 
-            try:
-              command = f'gcloud container node-pools list --cluster {results["name"]} --project {name} --format="value(name)" --region {results["location"]}'
-              process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-              output, err = process.communicate()
+        for x in output:
+          print(f'Cluster Name: {x["name"]}')
 
-              if output:
-                if results["num_nodes"]:
-                  print(f'GKE COS cluster {results["name"]} in project {name} has {results["num_nodes"]} node/s')
-                  total_cos_nodes += int(results["num_nodes"])
-                else:
-                  print(f'GKE COS cluster {results["name"]} in project {name} has no node/s')
-              elif err:
-                if results["num_nodes"]:
-                  print(f'GKE Autopilot cluster {results["name"]} in project {name} has {results["num_nodes"]} node/s')
-                  total_autopilot_nodes += int(results["num_nodes"])
-                else:
-                  print(f'GKE Autopilot cluster {results["name"]} in project {name} has no node/s')
-            except subprocess.CalledProcessError as e:
-              print(f'Error: {e}')
+          if 'autopilot' in x and 'enabled' in x['autopilot']:
+            print(x['name'], 'is a GKE Autopilot cluster')
+
+            if 'currentNodeCount' in x:
+              total_autopilot_nodes += int(x['currentNodeCount'])
+              print(f'Current Node Count {x["currentNodeCount"]}\n')
+            else:
+              print(f'Cluster {x["name"]} has no nodes\n')
+          else:
+            print(x['name'], 'is a GKE Standard cluster')
+
+            if 'currentNodeCount' in x:
+              total_standard_nodes += int(x['currentNodeCount'])
+              print(f'Current Node Count {x["currentNodeCount"]}\n')
+            else:
+              print(f'Cluster {x["name"]} has 0 nodes\n')
       except subprocess.CalledProcessError as err:
-        print(f"Error processing cluster information for project {name}: {err.output}")
+        print(f"Error processing cluster information for project {project}: {err.output}\n")
+      except json.decoder.JSONDecodeError as err:
+        print(f'Kubernetes Engine API has not been used in project {project}\n')
+      except Exception as err:
+        print(f'Generic error: {err}\n')
 
-    print(f'\n\nTotal GKE COS nodes are {total_cos_nodes}')
-    print(f'Total GKE Autopilot nodes are {total_autopilot_nodes}')
+    print('##########################\n')
+    print(f'Total GKE Standard Nodes: {total_standard_nodes}')
+    print(f'Total GKE Autopilot Nodes: {total_autopilot_nodes}')
   except RuntimeError as err:
     print(f"An error occurred: {err}")
 
